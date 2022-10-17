3e7adef967ba889958c04ec6583576377a85c02d
eriktamano <eriktamano@gmail.com>
2022-10-17 14:42:22 +0200

    Keyboard added

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from selenium import webdriver
+from initVariables import *
+import time
 
 # create webdriver object
 driver = webdriver.Chrome()
 # get google.co.in
-driver.get("https://github.com/")
+driver.get("https://wordlegame.org/")
+keyboard = getKeyboard(driver)
+
+panels = getPanels(driver)
+
+for i in range(0, 4):
+    print(str(i).center(20, "-"))
+    for panel_row in panels:
+        for panel in panel_row:
+            print(panel.text, end="")
+        print()
+    keyboard["b"].click()
+    keyboard["r"].click()
+    keyboard["e"].click()
+    keyboard["a"].click()
+    keyboard["d"].click()
+    time.sleep(1)
+
